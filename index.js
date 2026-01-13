853a4f59168d43f234f6f341351a534439a2dde9
Muhammad Zohaib <zohaibch763@gmail.com>
2026-01-13 17:43:12 +0500

    Updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://admin:admin@cluster0.mongodb.net/ubereats_clone?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://euber5676_db_user:80D50000@cluster0.llhgsxn.mongodb.net/";
 
 console.log('Starting backend server...');
 
